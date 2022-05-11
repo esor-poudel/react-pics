66d78d2c398491fed670d3585c207e634e3dbaef
ishwor <ishwor.poudel@ekbana.info>
2022-05-11 10:51:42 +0545

    initial commit

---
+++ src/api/Unsplash.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+export default axios.create({
+    baseURL: 'https://api.unsplash.com',
+    headers:{
+        Authorization: 'Client-ID RekiXVvM9EZEKV2Zd7FRoY0XutgrOIMkFf5sGiRAIAM'
+    }
+});
+
+
+
+
+
+
---
+++ src/components/ImageList.js
@@ -0,0 +1,15 @@
+import './ImageList.css';
+import React from "react";
+import ImageCard from './ImageCard';
+
+
+const ImageList = (props)=>{
+  const imagesList =  props.images.map((image)=>{
+        return <ImageCard key={image.id} image={image} />
+    });
+    return (
+        <div className="image-list">{imagesList}</div>
+    );
+}
+
+export default ImageList;
